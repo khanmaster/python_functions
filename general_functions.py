c9c7b6d4f03a1b8fe02c5d0399f12e05af7e3bbd
khanmaster <eashkhan@hotmail.com>
2020-05-27 14:54:36 +0100

    first functions

---
+++ general_functions.py
@@ -0,0 +1,5 @@
+# in python def is used to define the function and give it a name
+def print_something():
+# Python interpreter recognises intendation is key
+    print("Hello World")
+print_something()
